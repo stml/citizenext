6d77f53f50360b6ac18b13fe591512c74c07f28d
stml <james@shorttermmemoryloss.com>
2015-02-09 23:12:48 +0000

    Moved all the shit around for testing

--- testing/tester.js
+++ testing/tester.js
@@ -1,12 +1,12 @@
 var ip;
-var browsing_site;
+var browsing_site = 'http://tumblr.com';
 var my_location;
 var browsing_location;
+var nav_location;
 var mapbox_token = "pk.eyJ1Ijoic3RtbCIsImEiOiJDQ1FDcFNVIn0.C7ThVrFnQ7a7COlJe8tARw";
 var mapbox_project_sat_streets = 'stml.l62p34g6';
 
 $( document ).ready(function() {
-	browsing_site = 'http://google.com';
 	$('#browsing_site').html(browsing_site);
 	getMyLocation();
 	getBrowsingLocation(browsing_site);
@@ -18,10 +18,11 @@ $( document ).ready(function() {
 	});
 
 function getNavPosition(position) {
-    $('#nav_location').html((Math.round(position.coords.latitude * 10000) / 10000)+','+ (Math.round(position.coords.longitude * 10000) / 10000));
-    $('#nav_map').html('<img src="http://api.tiles.mapbox.com/v4/'+mapbox_project_sat_streets+'/'+position.coords.longitude+','+position.coords.latitude+',14/500x100.png32?access_token='+mapbox_token+'" />');
-    $.getJSON('http://nominatim.openstreetmap.org/reverse?format=json&lat='+position.coords.latitude+'&lon='+position.coords.longitude, function (result, textStatus, jqXHR) {
-    	$('#nav_address').html(result.display_name);
+	nav_location = position;
+    $('#nav_location').html((Math.round(nav_location.coords.latitude * 10000) / 10000)+','+ (Math.round(nav_location.coords.longitude * 10000) / 10000));
+    $('#nav_map').html('<img src="http://api.tiles.mapbox.com/v4/'+mapbox_project_sat_streets+'/'+nav_location.coords.longitude+','+nav_location.coords.latitude+',12/500x100.png32?access_token='+mapbox_token+'" />');
+    $.getJSON('http://nominatim.openstreetmap.org/reverse?format=json&lat='+nav_location.coords.latitude+'&lon='+nav_location.coords.longitude, function (result, textStatus, jqXHR) {
+    	$('#nav_address').html(result.display_name+' <img src="flags/flat/16/'+result.address.country_code.toUpperCase()+'.png" />');
     	});
 	}
 
@@ -35,8 +36,8 @@ function getMyLocation() {
         my_location = result;
         //console.log(my_location);
         $('#my_ip').html(my_location.ip);
-      	$('#my_location').html(my_location.city+', '+my_location.region_name+', '+my_location.country_name+' <img src="../flags/flat/16/'+my_location.country_code+'.png" />');
-      	$('#my_map').html('<img src="http://api.tiles.mapbox.com/v4/'+mapbox_project_sat_streets+'/'+my_location.longitude+','+my_location.latitude+',14/500x100.png32?access_token='+mapbox_token+'" />');
+      	$('#my_location').html(my_location.city+', '+my_location.region_name+', '+my_location.country_name+' <img src="flags/flat/16/'+my_location.country_code+'.png" />');
+      	$('#my_map').html('<img src="http://api.tiles.mapbox.com/v4/'+mapbox_project_sat_streets+'/'+my_location.longitude+','+my_location.latitude+',12/500x100.png32?access_token='+mapbox_token+'" />');
       	});	
 	}
 	
@@ -45,7 +46,7 @@ function getBrowsingLocation(iphostname) {
 		browsing_location = result;
 		console.log(browsing_location);
 	    $('#browsing_ip').html(browsing_location.ip);
-      	$('#browsing_location').html(browsing_location.city+', '+browsing_location.region_name+', '+browsing_location.country_name+' <img src="../flags/flat/16/'+browsing_location.country_code+'.png" />');	
-      	$('#browsing_map').html('<img src="http://api.tiles.mapbox.com/v4/'+mapbox_project_sat_streets+'/'+browsing_location.longitude+','+browsing_location.latitude+',14/500x100.png32?access_token='+mapbox_token+'" />');
+      	$('#browsing_location').html(browsing_location.city+', '+browsing_location.region_name+', '+browsing_location.country_name+' <img src="flags/flat/16/'+browsing_location.country_code+'.png" />');	
+      	$('#browsing_map').html('<img src="http://api.tiles.mapbox.com/v4/'+mapbox_project_sat_streets+'/'+browsing_location.longitude+','+browsing_location.latitude+',12/500x100.png32?access_token='+mapbox_token+'" />');
 		});
 	}
