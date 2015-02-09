80d5344fe0f4974856bc0ebc8ba9f018ec6cc6f3
stml <james@shorttermmemoryloss.com>
2015-02-09 20:08:55 +0000

    Added flags and local IP

--- testing/tester.js
+++ testing/tester.js
@@ -1,5 +1,6 @@
 var ip;
 var currentlybrowsing;
+var mylocation;
 
 $( document ).ready(function() {
 	currentlybrowsing = 'http://google.com';
@@ -11,5 +12,18 @@ function getIP() {
 	$.getJSON("http://api.ipify.org?format=jsonp&callback=?", function(json) {
         ip = json.ip;
       	$('#currentip').html(ip);
+   		getLocation(ip);
       	});
+	}
+	
+// This function takes an IP or a hostname.
+function getLocation(iphostname) {
+	$.getJSON('http://freegeoip.net/json/?callback=?', function (result, textStatus, jqXHR) {
+		//console.log("callback running");
+        //console.log(textStatus);
+        //console.log(jqXHR);
+        mylocation = result;
+        //console.log(mylocation);
+      	$('#currentlocation').html(mylocation.city+', '+mylocation.region_name+', '+mylocation.country_name+' <img src="../flags/flat/24/'+mylocation.country_code+'.png" />');
+      	});	
 	}
